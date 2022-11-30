a58d70f2453d9112c7f304af5370ff6db3a314bc
Clausan Liano <clausanliano@yahoo.com.br>
2022-11-30 13:32:44 -0300

    Correção do cabeçalho do Card da Tabela

--- resources/views/fabricante/index.blade.php
+++ resources/views/fabricante/index.blade.php
@@ -8,7 +8,7 @@
         <div class="alert alert-success">{{ session('mensagem') }}</div>
     @endif
     <div class="card card-dark">
-        <div class="card-header"></div>
+        <div class="card-header">
             Fabricantes
         </div>
         <div class="card-body">
